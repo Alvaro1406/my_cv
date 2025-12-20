fb4ce050a7addaef47014659291d9ef4b6d9b1cf
Alvaro Beruvides <alvaroberuvides@gmail.com>
2025-12-20 16:47:46 -0500

    Fix: The database seeding has been improved.

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -16,10 +16,8 @@ async function main() {
   console.log("🌱 Hash the password");
   const passwordHash = await bcrypt.hash("Qazxsw.12", 10);
 
-  // Create a top-level user
-  console.log("🌱 Create a top-level user");
-  await prisma.user.create({
-    data: {
+  const users = [
+    {
       username: "admin",
       password: passwordHash,
       firstName: "Alvaro",
@@ -30,7 +28,47 @@ async function main() {
       isActive: true,
       visible: true,
     },
-  });
+    {
+      username: "israel",
+      password: passwordHash,
+      firstName: "Israel",
+      lastName: "Beruvides",
+      phoneNumber: "+5354220883",
+      email: "abguzman1406@gmail.com",
+      role: "USER",
+      isActive: true,
+      visible: true,
+    },
+    {
+      username: "lizett",
+      password: passwordHash,
+      firstName: "Lizett",
+      lastName: "Guzmán",
+      phoneNumber: "+5351748028",
+      email: "paraisoscuba2@gmail.com",
+      role: "USER",
+      isActive: true,
+      visible: true,
+    },
+  ];
+
+  // Create a top-level user
+  console.log("🌱 Create users");
+  for (const item of users) {
+    await prisma.user.create({
+      data: {
+        username: item.username,
+        password: item.password,
+        firstName: item.firstName,
+        lastName: item.lastName,
+        phoneNumber: item.phoneNumber,
+        email: item.email,
+        role: "ADMIN",
+        isActive: item.isActive,
+        visible: item.visible,
+      },
+    });
+  }
 }
 
 main()
