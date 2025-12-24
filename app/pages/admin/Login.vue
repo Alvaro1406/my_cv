02a2ce9cbfd9622c263cb6598785d0d8947f191d
Alvaro Beruvides <alvaroberuvides@gmail.com>
2025-12-24 12:52:46 -0500

    Add: Admin login page.

---
+++ app/pages/admin/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex items-center justify-center w-[100%] h-[100dvh]">
+    <div
+      class="xs:w-[90%] sm:w-[80vh] h-auto bg-white z-999 p-8 my-12 rounded-lg shadow-lg"
+    >
+      <h1 class="text-2xl font-bold text-center text-primary">
+        Iniciar Sesión Admin
+      </h1>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  layout: "auth-admin",
+});
+</script>
