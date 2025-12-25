51430906a128460837b56b928fb7caf715beab6b
Alvaro Beruvides <alvaroberuvides@gmail.com>
2025-12-25 10:21:06 -0500

    Add: Button primary component.

---
+++ app/components/default/ButtonPrimary.vue
@@ -0,0 +1,13 @@
+<template>
+  <Button type="button" :label="label" :icon="icon" :loading="loading" />
+</template>
+
+<script setup lang="ts">
+import Button from "primevue/button";
+
+defineProps<{
+  loading?: boolean;
+  label?: string;
+  icon?: string;
+}>();
+</script>
