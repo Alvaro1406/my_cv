719adba7b62c87ce4364c09fc3d9fc8609b9d5e9
Alvaro Beruvides <alvaroberuvides@gmail.com>
2026-01-05 12:22:43 -0500

    Fix: The menu has been restructured in the sidebar menu.

---
+++ app/utils/colors.ts
@@ -0,0 +1,25 @@
+export const useColors = () => {
+  const colors = [
+    "red",
+    "orange",
+    "amber",
+    "yellow",
+    "lime",
+    "green",
+    "emerald",
+    "teal",
+    "cyan",
+    "sky",
+    "blue",
+    "indigo",
+    "violet",
+    "purple",
+    "fuchsia",
+    "pink",
+    "rose",
+  ];
+
+  const neutrals = ["slate", "gray", "zinc", "neutral", "stone"];
+
+  return { colors, neutrals };
+};
