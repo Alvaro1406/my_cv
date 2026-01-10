2d6c0a67c115b857961988dd345705fa82f47471
Alvaro <alvaroberuvides@gmail.com>
2026-01-10 11:58:17 -0500

    Add: Logout function to auth compossable.

--- app/composables/auth.ts
+++ app/composables/auth.ts
@@ -2,11 +2,17 @@ import type { IErrorCatch } from "~/types/error-catch";
 import type { IAuthCredentials, IAuthResponse } from "~/types/auth";
 
 export const useAuth = () => {
+  /** Properties */
   const message = ref<string>("");
   const loading = ref<boolean>(false);
 
+  /** Import router */
   const router = useRouter();
 
+  /**
+   * Login function
+   * @param userData username, password
+   */
   async function login(userData: IAuthCredentials) {
     message.value = "";
     loading.value = true;
@@ -31,8 +37,32 @@ export const useAuth = () => {
     }
   }
 
+  /**
+   * Logout function
+   */
+  async function logout() {
+    loading.value = true;
+    try {
+      const response = await $fetch("/api/auth/login", {
+        method: "POST",
+      });
+
+      if (response.success) {
+        if (import.meta.client) {
+          localStorage.removeItem("auth-token");
+        }
+        router.push("/login");
+      }
+      loading.value = false;
+    } catch (error: IErrorCatch | any) {
+      message.value = error.response._data?.message;
+      loading.value = false;
+    }
+  }
+
   return {
     login,
+    logout,
     loading,
     message,
   };
