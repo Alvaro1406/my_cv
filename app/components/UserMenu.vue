ae34c94b5a5bddc5a1647b0deded44ecc95e163f
Alvaro <alvaroberuvides@gmail.com>
2026-01-13 10:01:16 -0500

    Fix: Bugs in user menu components.

--- app/components/UserMenu.vue
+++ app/components/UserMenu.vue
@@ -1,132 +1,9 @@
 <script setup lang="ts">
-import type { DropdownMenuItem } from "@nuxt/ui";
-
 defineProps<{
   collapsed?: boolean;
 }>();
 
-const colorMode = useColorMode();
-const appConfig = useAppConfig();
-
-const { colors, neutrals } = useColors();
-
-const user = ref({
-  name: "Benjamin Canac",
-  avatar: {
-    src: "https://github.com/benjamincanac.png",
-    alt: "Benjamin Canac",
-  },
-});
-
-const items = computed<DropdownMenuItem[][]>(() => [
-  [
-    {
-      type: "label",
-      label: user.value.name,
-      avatar: user.value.avatar,
-    },
-  ],
-  [
-    {
-      label: "Profile",
-      icon: "i-lucide-user",
-    },
-  ],
-  [
-    {
-      label: "Apariencia",
-      icon: "i-lucide-palette",
-      children: [
-        {
-          label: "Primario",
-          slot: "chip",
-          chip: appConfig.ui.colors.primary,
-          content: {
-            align: "center",
-            collisionPadding: 16,
-          },
-          children: colors.map((color) => ({
-            label: color,
-            chip: color,
-            slot: "chip",
-            checked: appConfig.ui.colors.primary === color,
-            type: "checkbox",
-            onSelect: (e) => {
-              e.preventDefault();
-
-              appConfig.ui.colors.primary = color;
-            },
-          })),
-        },
-        {
-          label: "Neutral",
-          slot: "chip",
-          chip:
-            appConfig.ui.colors.neutral === "neutral"
-              ? "old-neutral"
-              : appConfig.ui.colors.neutral,
-          content: {
-            align: "end",
-            collisionPadding: 16,
-          },
-          children: neutrals.map((color) => ({
-            label: color,
-            chip: color === "neutral" ? "old-neutral" : color,
-            slot: "chip",
-            type: "checkbox",
-            checked: appConfig.ui.colors.neutral === color,
-            onSelect: (e) => {
-              e.preventDefault();
-
-              appConfig.ui.colors.neutral = color;
-            },
-          })),
-        },
-      ],
-    },
-    {
-      label: "Tema",
-      icon: "i-lucide-sun-moon",
-      children: [
-        {
-          label: "Light",
-          icon: "i-lucide-sun",
-          type: "checkbox",
-          checked: colorMode.value === "light",
-          onSelect(e: Event) {
-            e.preventDefault();
-
-            colorMode.preference = "light";
-          },
-        },
-        {
-          label: "Dark",
-          icon: "i-lucide-moon",
-          type: "checkbox",
-          checked: colorMode.value === "dark",
-          onUpdateChecked(checked: boolean) {
-            if (checked) {
-              colorMode.preference = "dark";
-            }
-          },
-          onSelect(e: Event) {
-            e.preventDefault();
-          },
-        },
-      ],
-    },
-  ],
-  [
-    {
-      label: "Cerrar sesión",
-      icon: "i-lucide-log-out",
-      onSelect: () => {
-        /* Handle logout */
-        console.log("Logging out...");
-      },
-    },
-  ],
-]);
+const { user, items } = useMenuUser();
 </script>
 
 <template>
@@ -140,7 +17,15 @@ const items = computed<DropdownMenuItem[][]>(() => [
     <UButton
       v-bind="{
         ...user,
-        label: collapsed ? undefined : user?.name,
+        label: collapsed
+          ? undefined
+          : !user.firstName || !user.lastName
+          ? 'Cargando...'
+          : `${user.firstName} ${user.lastName}`,
+        avatar: {
+          src: user.image,
+          alt: user.firstName + '' + user.lastName,
+        },
         trailingIcon: collapsed ? undefined : 'i-lucide-chevrons-up-down',
       }"
       color="neutral"
---
+++ app/utils/user-menu.ts
@@ -0,0 +1,138 @@
+import type { IUser } from "~~/types/user";
+import type { DropdownMenuItem } from "@nuxt/ui";
+
+export const useMenuUser = () => {
+  const colorMode = useColorMode();
+  const appConfig = useAppConfig();
+
+  const { colors, neutrals } = useColors();
+
+  const { logout } = useAuth();
+
+  const user = computed<IUser>(() => {
+    if (import.meta.client) {
+      const data: string | null | IUser = localStorage.getItem("user");
+      return JSON.parse(data as string) as IUser;
+    } else {
+      return {} as IUser;
+    }
+  });
+
+  const items = computed<DropdownMenuItem[][]>(() => [
+    [
+      {
+        type: "label",
+        label: user.value.firstName + " " + user.value.lastName,
+        avatar: {
+          src: "https://zhtzkllbjojohejpmocw.supabase.co/storage/v1/object/public/my-cv-bucket/users/b71b3514-4fcd-4991-b18a-af48d3d83cd9.jpg",
+          alt: user.value.firstName + " " + user.value.lastName,
+        },
+      },
+    ],
+    [
+      {
+        label: "Perfil",
+        icon: "i-lucide-user",
+      },
+    ],
+    [
+      {
+        label: "Apariencia",
+        icon: "i-lucide-palette",
+        children: [
+          {
+            label: "Primario",
+            slot: "chip",
+            chip: appConfig.ui.colors.primary,
+            content: {
+              align: "center",
+              collisionPadding: 16,
+            },
+            children: colors.map((color) => ({
+              label: color,
+              chip: color,
+              slot: "chip",
+              checked: appConfig.ui.colors.primary === color,
+              type: "checkbox",
+              onSelect: (e) => {
+                e.preventDefault();
+
+                appConfig.ui.colors.primary = color;
+              },
+            })),
+          },
+          {
+            label: "Neutral",
+            slot: "chip",
+            chip:
+              appConfig.ui.colors.neutral === "neutral"
+                ? "old-neutral"
+                : appConfig.ui.colors.neutral,
+            content: {
+              align: "end",
+              collisionPadding: 16,
+            },
+            children: neutrals.map((color) => ({
+              label: color,
+              chip: color === "neutral" ? "old-neutral" : color,
+              slot: "chip",
+              type: "checkbox",
+              checked: appConfig.ui.colors.neutral === color,
+              onSelect: (e) => {
+                e.preventDefault();
+
+                appConfig.ui.colors.neutral = color;
+              },
+            })),
+          },
+        ],
+      },
+      {
+        label: "Tema",
+        icon: "i-lucide-sun-moon",
+        children: [
+          {
+            label: "Light",
+            icon: "i-lucide-sun",
+            type: "checkbox",
+            checked: colorMode.value === "light",
+            onSelect(e: Event) {
+              e.preventDefault();
+
+              colorMode.preference = "light";
+            },
+          },
+          {
+            label: "Dark",
+            icon: "i-lucide-moon",
+            type: "checkbox",
+            checked: colorMode.value === "dark",
+            onUpdateChecked(checked: boolean) {
+              if (checked) {
+                colorMode.preference = "dark";
+              }
+            },
+            onSelect(e: Event) {
+              e.preventDefault();
+            },
+          },
+        ],
+      },
+    ],
+    [
+      {
+        label: "Cerrar sesión",
+        icon: "i-lucide-log-out",
+        onSelect: async () => {
+          /* Handle logout */
+          await logout();
+        },
+      },
+    ],
+  ]);
+
+  return {
+    user,
+    items,
+  };
+};
