8a55a6c7b78da91bc97f574efaf1f08a0a7c221a
Alvaro Beruvides <alvaroberuvides@gmail.com>
2026-09-03 21:52:12 -0300

    Add: New button component.

---
+++ app/components/components/buttonCmp.vue
@@ -0,0 +1,42 @@
+<template>
+  <UButton
+    :label="label"
+    :color="color"
+    :variant="variant"
+    :size="size"
+    :icon="icon"
+    :trailing-icon="trailingIcon"
+    :active="active"
+    :active-class="activeClass"
+    :disabled="disabled"
+  />
+</template>
+
+<script setup lang="ts">
+import type { ButtonProps } from "@nuxt/ui";
+
+withDefaults(
+  defineProps<{
+    label?: string;
+    color?: ButtonProps["color"];
+    variant?: ButtonProps["variant"];
+    size?: ButtonProps["size"];
+    icon?: string;
+    trailingIcon?: string;
+    active?: boolean;
+    activeClass?: string;
+    disabled?: boolean;
+  }>(),
+  {
+    label: "Button",
+    color: "primary",
+    variant: "outline",
+    size: "xl",
+    icon: "",
+    trailingIcon: "",
+    active: false,
+    activeClass: "",
+    disabled: false,
+  },
+);
+</script>
