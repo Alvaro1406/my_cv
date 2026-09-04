bdfb2c370a171e41672ef744025b4d8804594c06
Alvaro Beruvides <alvaroberuvides@gmail.com>
2026-09-03 21:15:17 -0300

    Update: Information in the database has been updated.

--- prisma/data-seed/technical-mastery.ts
+++ prisma/data-seed/technical-mastery.ts
@@ -6,7 +6,16 @@ export const technicalMastery = [
       en: "Specialist in reactive and high-performance interfaces using modern ecosystems.",
       pt: "Especialista em interfaces reativas e de alto desempenho utilizando ecossistemas modernos.",
     },
-    tags: ["VUEJS", "NUXT JS", "JAVASCRIPT", "TYPESCRIPT", "VITE"],
+    tags: [
+      "JAVASCRIPT",
+      "TYPESCRIPT",
+      "VUE",
+      "NUXT",
+      "VITE",
+      "REACT",
+      "NEXT",
+      "WEBPACK",
+    ],
     image:
       "https://zhtzkllbjojohejpmocw.supabase.co/storage/v1/object/public/my-cv-bucket/tech-mastery/panel.svg",
   },
@@ -17,7 +26,7 @@ export const technicalMastery = [
       en: "Scalable design systems and modular components with a focus on user experience.",
       pt: "Sistemas de design escaláveis e componentes modulares com foco na experiência do usuário.",
     },
-    tags: ["TAILWIND CSS", "QUASAR", "PRIMEVUE", "FIGMA"],
+    tags: ["TAILWIND CSS", "PRIMEVUE", "QUASAR", "FIGMA"],
     image:
       "https://zhtzkllbjojohejpmocw.supabase.co/storage/v1/object/public/my-cv-bucket/tech-mastery/design.svg",
   },
@@ -28,7 +37,7 @@ export const technicalMastery = [
       en: "Mastery of collaboration tools, version control, and professional development environments.",
       pt: "Domínio de ferramentas de colaboração, controle de versão e ambientes de desenvolvimento profissionais.",
     },
-    tags: ["VS CODE", "POSTMAN", "GIT", "JIRA", "CONFLUENCE"],
+    tags: ["VS CODE", "POSTMAN", "GIT", "JIRA", "CONFLUENCE", "TRELLO"],
     image:
       "https://zhtzkllbjojohejpmocw.supabase.co/storage/v1/object/public/my-cv-bucket/tech-mastery/terminal.svg",
   },
